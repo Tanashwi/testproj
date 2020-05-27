476fc77b6702dcc32aabe694072fad3d1620caeb
Tanmaya Thopate <tanmaya.thopate@ensono.com>
2020-05-27 17:59:21 +0530

    moved to separate files of fixed values.

---
+++ ConsoleApp1/ConsoleApp1/Classes/FixedValues.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1.Classes
+{
+    public class FixedValues
+    {
+        private static string[] firstDigits = {
+                       "", "One ", "Two ", "Three ", "Four ",
+                       "Five ", "Six ", "Seven ", "Eight ",
+                       "Nine ", "Ten ", "Eleven ", "Twelve ",
+                       "Thirteen ", "Fourteen ", "Fifteen ",
+                       "Sixteen ", "Seventeen ", "Eighteen ",
+                       "Nineteen "};
+
+        //left first 2 empty in array, for handling values
+        private static string[] secondDigits = {"", "", "Twenty ", "Thirty ", "Forty ",
+                       "Fifty ", "Sixty ", "Seventy ", "Eighty ",
+                       "Ninety "};
+
+        public static string[] FirstDigits { get => firstDigits; set => firstDigits = value; }
+        public static string[] SecondDigits { get => secondDigits; set => secondDigits = value; }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp1.Classes;
+using System;
 
 namespace ConsoleApp1
 {
@@ -10,27 +11,18 @@ namespace ConsoleApp1
             Console.WriteLine("Enter numeric value less than or equal to 99,999 only. No alphabets.");
             var inputField = Console.ReadLine();
             if (inputField.ToString().Length > 5)
+            {
                 Console.WriteLine("Enter value less than 5, means conversion is implemented till 99,999 only.");
-            //We can implement it, but pdf asked till 10 thousand. We will need to go on adding conditions in implemented class.
-            //Right now, it is assumed that, user will enter only NUMERIC fields and not alphabets.
-            Console.WriteLine("Enter once Again:");
-            inputField = Console.ReadLine();
-
+                //We can implement it, but pdf asked till 10 thousand. We will need to go on adding conditions in implemented class.
+                //Right now, it is assumed that, user will enter only NUMERIC fields and not alphabets.
+                Console.WriteLine("Enter once Again:");
+                inputField = Console.ReadLine();
+            }
             Console.WriteLine(ConvertToString(Convert.ToInt32(inputField)));
             Console.ReadKey();
         }
 
-        static string[] firstDigits = {
-                       "", "one ", "two ", "three ", "four ",
-                       "five ", "six ", "seven ", "eight ",
-                       "nine ", "ten ", "eleven ", "twelve ",
-                       "thirteen ", "fourteen ", "fifteen ",
-                       "sixteen ", "seventeen ", "eighteen ",
-                       "nineteen "};
-        //left first 2 empty in array, for handling values
-        static string[] secondDigits = {"", "", "twenty ", "thirty ", "forty ",
-                       "fifty ", "sixty ", "seventy ", "eighty ",
-                       "ninety "};
+
 
 
         static string ConvertToString(int inputParam)
@@ -53,12 +45,12 @@ namespace ConsoleApp1
 
             if (v > 19)
             {
-                finalString += secondDigits[v / 10] + firstDigits[v % 10];
+                finalString += FixedValues.SecondDigits[v / 10] + FixedValues.FirstDigits[v % 10];
 
             }
             else
             {
-                finalString += firstDigits[v];
+                finalString += FixedValues.FirstDigits[v];
             }
 
             if (v != 0)
