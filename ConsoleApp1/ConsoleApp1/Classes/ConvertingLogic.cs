d9c76c7fcca5ce98f042b24b8a443c38653422a7
Tanmaya Thopate <tanmaya.thopate@ensono.com>
2020-05-27 18:03:56 +0530

    Reduced file by dividing code into different files

---
+++ ConsoleApp1/ConsoleApp1/Classes/ConvertingLogic.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp1.Classes
+{
+    class ConvertingLogic
+    {
+        public static string ConvertToString(int inputParam)
+        {
+
+            string outputString = "";
+
+            outputString += ConvertFunToString((int)((inputParam / 1000) % 100), "Thousand ");
+
+            outputString += ConvertFunToString((int)((inputParam / 100) % 10), "Hundred ");
+
+            outputString += ConvertFunToString((int)(inputParam % 100), "");
+
+            return outputString;
+        }
+
+        private static string ConvertFunToString(int v, string positionValue)
+        {
+            string finalString = "";
+
+            if (v > 19)
+            {
+                finalString += FixedValues.SecondDigits[v / 10] + FixedValues.FirstDigits[v % 10];
+
+            }
+            else
+            {
+                finalString += FixedValues.FirstDigits[v];
+            }
+
+            if (v != 0)
+            {
+                finalString += positionValue;
+            }
+
+            return finalString;
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,7 +7,6 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Enter numeric value less than or equal to 99,999 only. No alphabets.");
             var inputField = Console.ReadLine();
             if (inputField.ToString().Length > 5)
@@ -18,47 +17,8 @@ namespace ConsoleApp1
                 Console.WriteLine("Enter once Again:");
                 inputField = Console.ReadLine();
             }
-            Console.WriteLine(ConvertToString(Convert.ToInt32(inputField)));
+            Console.WriteLine("\n" + inputField + " is splelled as:" + ConvertingLogic.ConvertToString(Convert.ToInt32(inputField)));
             Console.ReadKey();
-        }
-
-
-
-
-        static string ConvertToString(int inputParam)
-        {
-
-            string outputString = "";
-
-            outputString += ConvertFunToString((int)((inputParam / 1000) % 100), "Thousand ");
-
-            outputString += ConvertFunToString((int)((inputParam / 100) % 10), "Hundred ");
-
-            outputString += ConvertFunToString((int)(inputParam % 100), "");
-
-            return outputString;
-        }
-
-        private static string ConvertFunToString(int v, string positionValue)
-        {
-            string finalString = "";
-
-            if (v > 19)
-            {
-                finalString += FixedValues.SecondDigits[v / 10] + FixedValues.FirstDigits[v % 10];
-
-            }
-            else
-            {
-                finalString += FixedValues.FirstDigits[v];
-            }
-
-            if (v != 0)
-            {
-                finalString += positionValue;
-            }
-
-            return finalString;
         }
     }
 }
