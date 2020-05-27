2c12752214b55ba14f73cb5f2950fe96223046f6
Tanmaya Thopate <tanmaya.thopate@ensono.com>
2020-05-27 17:51:11 +0530

    input limit and commented code in some places

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Enter numeric value less than or equal to 99,999 only. No alphabets.");
             var inputField = Console.ReadLine();
+            if (inputField.ToString().Length > 5)
+                Console.WriteLine("Enter value less than 5, means conversion is implemented till 99,999 only.");
+            //We can implement it, but pdf asked till 10 thousand. We will need to go on adding conditions in implemented class.
+            //Right now, it is assumed that, user will enter only NUMERIC fields and not alphabets.
+            Console.WriteLine("Enter once Again:");
+            inputField = Console.ReadLine();
+
             Console.WriteLine(ConvertToString(Convert.ToInt32(inputField)));
             Console.ReadKey();
         }
@@ -26,9 +35,8 @@ namespace ConsoleApp1
 
         static string ConvertToString(int inputParam)
         {
+
             string outputString = "";
-
-
 
             outputString += ConvertFunToString((int)((inputParam / 1000) % 100), "Thousand ");
 
