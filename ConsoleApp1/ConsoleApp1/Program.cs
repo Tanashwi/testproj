754f4b8af783318b060b4cfae755456a357f0da3
Tanmaya Thopate <tanmaya.thopate@ensono.com>
2020-05-27 17:23:11 +0530

    tried first ten

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -10,6 +6,35 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            var inputField = Console.ReadLine();
+            Console.WriteLine(ConvertToString(Convert.ToInt32(inputField)));
+            Console.ReadKey();
+        }
+
+        static string[] firstDigits = {
+                       "", "one ", "two ", "three ", "four ",
+                       "five ", "six ", "seven ", "eight ",
+                       "nine ", "ten ", "eleven ", "twelve ",
+                       "thirteen ", "fourteen ", "fifteen ",
+                       "sixteen ", "seventeen ", "eighteen ",
+                       "nineteen "};
+
+        static string ConvertToString(int inputParam)
+        {
+            string outputString = "";
+
+            outputString = ConvertFunToString((int)(inputParam % 100));
+
+            return outputString;
+        }
+
+        private static string ConvertFunToString(int v)
+        {
+            string finalString = "";
+
+            finalString += firstDigits[v];
+
+            return finalString;
         }
     }
 }
